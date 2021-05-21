5283e18e66f9983196eabe56c7d28f15bfcbf7cb
chasscepts <francischassobetta@gmail.com>
2021-05-21 14:12:01 +0100

    Implement weather information display

--- src/index.js
+++ src/index.js
@@ -20,15 +20,65 @@ const getWeatherInfoByPosition = async (longitude, latitude) => {
   }
 };
 
+const displayWeather = (() => {
+  const icon = document.querySelector('#icon');
+  const city = document.querySelector('#city');
+  const country = document.querySelector('#country');
+  const longitude = document.querySelector('#longitude');
+  const latitude = document.querySelector('#latitude');
+  const temperature = document.querySelector('#temperature');
+  const maxTemp = document.querySelector('#max-temp');
+  const minTemp = document.querySelector('#min-temp');
+  const windSpeed = document.querySelector('#wind-speed');
+  const windDirection = document.querySelector('#wind-direction');
+  const description = document.querySelector('#description');
+  const clouds = document.querySelector('#clouds');
+  const humidity = document.querySelector('#humidity');
+
+  return (weather) => {
+    icon.src = weather.image;
+    city.textContent = weather.city;
+    country.textContent = weather.country;
+    longitude.textContent = weather.longitude;
+    latitude.textContent = weather.latitude;
+    temperature.textContent = weather.temperature;
+    maxTemp.textContent = weather.maxtemprature;
+    minTemp.textContent = weather.minTemperature;
+    windSpeed.textContent = weather.windSpeed;
+    windDirection.textContent = weather.windDirection;
+    description.textContent = weather.description;
+    clouds.textContent = weather.clouds;
+    humidity.textContent = weather.humidity;
+  };
+})();
+
+const sampleWeather = {
+  city: 'Ibadan',
+  clouds: 'Cloudiness: 83%',
+  country: 'NG',
+  description: 'Broken Clouds',
+  humidity: 'Humidity: 48%',
+  image: 'http://openweathermap.org/img/wn/04d@2x.png',
+  latitude: 7.4318,
+  longitude: 3.8994,
+  maxtemprature: 34,
+  minTemperature: 34,
+  temperature: 34,
+  windDirection: 'South West',
+  windSpeed: '9.6 km/h',
+};
+
+displayWeather(sampleWeather);
+
 // getWeatherInfo('London,uk');
 
-location.position()
-  .then((position) => {
-    console.log(position);
-    try {
-      getWeatherInfoByPosition(position.longitude, position.latitude);
-    } catch (err) {
-      console.log(err);
-    }
-  })
-  .catch((err) => console.log(err));
+// location.position()
+//   .then((position) => {
+//     console.log(position);
+//     try {
+//       getWeatherInfoByPosition(position.longitude, position.latitude);
+//     } catch (err) {
+//       console.log(err);
+//     }
+//   })
+//   .catch((err) => console.log(err));
--- src/open-weather-map.js
+++ src/open-weather-map.js
@@ -7,6 +7,25 @@ const getPositionUrl = (longitude, latitude) => `${baseUrl}?lat=${latitude}&lon=
 
 const iconUrl = (icon) => `http://openweathermap.org/img/wn/${icon}@2x.png`;
 
+const windDirection = (num) => {
+  num = parseInt(num, 10);
+  if (num === 0) return 'North';
+  if (num < 90) return 'North East';
+  if (num === 90) return 'East';
+  if (num < 180) return 'South East';
+  if (num === 180) return 'South';
+  if (num < 270) return 'South West';
+  if (num === 270) return 'West';
+  if (num < 360) return 'North West';
+  return 'North';
+};
+
+const convertTemperature = (temp) => Math.round(temp - 273.15);
+
+const convertSpeed = (speed) => `${(Math.round(speed * 36) / 10)} km/h`;
+
+const capitalize = (text) => text.split(' ').map((w) => `${w[0].toUpperCase()}${w.substring(1)}`).join(' ');
+
 const formatWeatherInfo = (raw) => (
   {
     city: raw.name,
@@ -14,13 +33,14 @@ const formatWeatherInfo = (raw) => (
     image: iconUrl(raw.weather[0].icon),
     longitude: raw.coord.lon,
     latitude: raw.coord.lat,
-    cloudSummary: raw.weather[0].main,
-    cloud: raw.weather[0].description,
-    temperature: raw.main.temp,
-    pressure: raw.main.pressure,
-    humidity: raw.main.humidity,
-    windSpeed: raw.wind.speed,
-    windDirection: raw.wind.deg,
+    description: capitalize(raw.weather[0].description),
+    clouds: `Cloudiness: ${raw.clouds.all}%`,
+    temperature: convertTemperature(raw.main.temp),
+    maxtemprature: convertTemperature(raw.main.temp_max),
+    minTemperature: convertTemperature(raw.main.temp_min),
+    humidity: `Humidity: ${raw.main.humidity}%`,
+    windSpeed: convertSpeed(raw.wind.speed),
+    windDirection: windDirection(raw.wind.deg),
   }
 );
 
